b621266be6ebc2e848454757e5a562aa8f8c0bc1
Piotr <37638629+tanfonto@users.noreply.github.com>
2022-10-30 12:11:22 +0100

    Update result.test.ts

--- test/result.test.ts
+++ test/result.test.ts
@@ -1,11 +1,11 @@
-import { valueOf } from "../lib/dsl";
+import { resume } from "../lib/dsl";
 import { Do } from "../lib/interpreter";
 import { result } from "../lib/result";
 
 function* R(): Do<number> {
   const x = yield Promise.resolve(42);
   const y = yield Promise.resolve(x + 8);
-  return valueOf(Promise.reject(Error("42")), y + 12);
+  return resume(Promise.reject(Error("42")), y + 12);
 }
 
 result(R).then(console.log);
